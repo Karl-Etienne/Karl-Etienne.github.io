a7f4da20a4b907578c93894c1c58233ed84cbf07
Karl-Etienne <Karl-Etienne.Perron@USherbrooke.ca>
2022-08-18 19:51:59 -0400

    Sc. 1,2,3

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,13 @@ function loadContent()
 {
     var scenarios = document.getElementById('newScenarios')
     
-    scenarios.insertAdjacentHTML('beforeend', '<a href="scenario.html"><button class="casperProblems" onclick="setScen(this)" onmouseenter="mouseOverEmphasis(this)" onmouseleave="stopMouseOverEmphasis(this)">0</button></a>')
+    scenarios.insertAdjacentHTML('beforeend', '<a href="scenario.html"><button class="casperProblems" onclick="setScen(this)" onmouseenter="mouseOverEmphasis(this)" onmouseleave="stopMouseOverEmphasis(this)">1</button></a>')
+    scenarios.insertAdjacentHTML('beforeend', '<a href="scenario.html"><button class="casperProblems" onclick="setScen(this)" onmouseenter="mouseOverEmphasis(this)" onmouseleave="stopMouseOverEmphasis(this)">2</button></a>')
+    scenarios.insertAdjacentHTML('beforeend', '<a href="scenario.html"><button class="casperProblems" onclick="setScen(this)" onmouseenter="mouseOverEmphasis(this)" onmouseleave="stopMouseOverEmphasis(this)">3</button></a>')
 
     scenarios = document.getElementById('legacyScenarios')
     
+    scenarios.insertAdjacentHTML('beforeend', '<a href="scenario.html"><button class="casperProblems" onclick="setScen(this)" onmouseenter="mouseOverEmphasis(this)" onmouseleave="stopMouseOverEmphasis(this)">0</button></a>')
     scenarios.insertAdjacentHTML('beforeend', '<a href="scenario.html"><button class="casperProblems" onclick="setScen(this)" onmouseenter="mouseOverEmphasis(this)" onmouseleave="stopMouseOverEmphasis(this)">99</button></a>')
 }
 
--- scripts/scenario.js
+++ scripts/scenario.js
@@ -129,6 +129,45 @@ function mouseOverEmphasis(button){button.style.color = "red"}
 function stopMouseOverEmphasis(button){button.style.color = ""}
 
 const scenarios = {
+    1: {
+        situation:"Vous êtes dans un bar avec 2 amis. La télévision affiche \
+        aux nouvelles que la police recherche activement les personnes responsables \
+        d'avoir déboulonné une statue d'un politicien historique perçu \
+        comme raciste. Votre premier ami s'exclame: \"Bienfait! J'aurais aimé y être pour participer!\" \
+        Le second semble surpris et répond sur un ton défensif: \"Es-tu sérieux? C'est un geste \
+        de pure violence et un grand manque de respect à notre nation, à notre histoire!\"",
+        q1: "Avez-vous déjà été témoin de racisme autour de vous? Comment pouvons-nous éliminer \
+         ce type de problème dans le milieu de la santé selon vous?",
+        q2: "Vos amis vous demandent votre opinion sur le sujet. À qui donnez-vous raison? Pourquoi?",
+        q3: "Une personne appeurée vous approche rapidement et vous parle, \
+        mais dans une langue complètement étrangère. Que faites-vous?"
+        },
+    2: {
+        situation:"Un beau jour d'été, vous êtes assis sur un banc aux abords d'une résidence \
+        pour personnes âgées. Il fait très chaud au soleil, mais heureusement, vous avez votre bouteille d'eau. \
+        Vous voyez une préposée sortir de l'établissement avec un résident en chaise roulante. Elle \
+        s'arrête un peu plus loin et sort son cellulaire. Elle embarque dans une discussion et \
+        porte peu attention au résident. Après un certain temps, vous entendez le résident: \"\
+        Madame! J'ai très soif, il fait chaud!\" Elle l'ignore. \"MADAME! J'ai vraiment soif!\" \
+        Elle lui répond rapidement: \"Aille! Vous pouvez pas boire d'eau sans surveillance et j'en ai pas! \
+        Profitez donc du soleil et laissez moi tranquille\". Le résident vous regarde et vous dit: \
+        \"S'il vous plaît! Pourriez-vous me laisser un peu de votre eau?\"",
+        q1: "Est-ce que vous donneriez de votre eau au résident?",
+        q2: "Selon vous, de quelle façon devrions-nous prendre soin nos personnes âgées au Québec?",
+        q3: "Si dans la situation vous étiez médecin, auriez-vous réagit de manière différente? Pourquoi?"
+        },
+    3: {
+        situation:"Vous travaillez dans une grande firme de jeux vidéo. La date de sortie de votre \
+        projet courant approche à grands pas, mais votre équipe se bute jours après jours à un problème \
+        majeur. Le jeu ne peut sortir sans avoir résolu ce problème. Un jour, un membre de votre équipe \
+        lance un grand soupir de satisfaction; il a résolu le problème!!! Il vous montre le jeu qui \
+        fonctionne alors sans erreur! Il soumet sa solution pour révision. Lors de la révision, \
+        on vous interpelle puisque le système a détecté qu'il s'agissait d'un plagiat ligne pour ligne \
+        d'une solution trouvée sur Stack Overflow...",
+        q1: "Si vous étiez \"team lead\", comment approcheriez-vous cette situation?",
+        q2: "Que faites vous pour assurer que vos projets se terminent dans les délais prévus?",
+        q3: "Le plagiaire devrait faire face à quels types de conséquences selon-vous?"
+        },
     99: {
         situation:"Test boboche",
         q1: "Blablabla Mr.Freeman",
@@ -140,5 +179,11 @@ const scenarios = {
         q1: "Vrai ou faux? Un vol peut être justifié.",
         q2: "Donnez-vous de la monnaie aux sans-abris que vous croisez dans la rue? Pourquoi?",
         q3: "Dans quelle(s) situation(s) pouvons-nous dire que \"la fin justifie les moyens?\""
-        }
+        },
+    999: {
+        situation:"",
+        q1: "",
+        q2: "",
+        q3: ""
+    }
     }
